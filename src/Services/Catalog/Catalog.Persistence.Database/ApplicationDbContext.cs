60d43ee96713ff84399eab6943c0f1810d67a695
mariobot <mariobot113@hotmail.com>
2020-10-02 17:48:27 -0500

    Chapter 21 Persistence and Domain Projects

---
+++ src/Services/Catalog/Catalog.Persistence.Database/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Catalog.Domain;
+
+namespace Catalog.Persistence.Database
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options): base(options) {
+            
+        }
+        
+        public DbSet<Product> Products { get; set; }
+    }
+}
