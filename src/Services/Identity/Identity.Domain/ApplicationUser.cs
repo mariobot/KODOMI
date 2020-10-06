f8c3b6c21b127edfadba47c1393bddcfbaa0ed9e
mariobot <mariobot113@hotmail.com>
2020-10-05 19:47:04 -0500

    Chapter 36 ASP.NET Identity Core

---
+++ src/Services/Identity/Identity.Domain/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identity.Domain
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public ICollection<ApplicationUserRole> UserRoles { get; set; }
+    }
+}
