58edeb32ba650c2f5eb2e98dd96a2b16f15db687
mariobot <mariobot113@hotmail.com>
2020-10-19 17:12:57 -0500

    Register HttpClient and running get request

---
+++ src/Client/Clients.Blazor/Models/ClientDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clients.Blazor.Models
+{
+    public class ClientDto
+    {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+    }
+}
--- src/Client/Clients.Blazor/Startup.cs
+++ src/Client/Clients.Blazor/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
@@ -8,7 +9,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Clients.Blazor.Data;
 
 namespace Clients.Blazor
 {
@@ -27,7 +27,12 @@ namespace Clients.Blazor
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+
+            services.AddScoped(sp =>
+            new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:10000/")
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
