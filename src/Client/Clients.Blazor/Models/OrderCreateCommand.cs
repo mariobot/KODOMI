165b2392045bd3ebc14061c7284ab5e1f7d6c649
mariobot <mariobot113@hotmail.com>
2020-10-19 17:55:57 -0500

    Orders, Clients and Products listened.

---
+++ src/Client/Clients.Blazor/Models/OrderCreateCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clients.Blazor.Models
+{
+    public class OrderCreateCommand
+    {
+        public int PaymentType { get; set; }
+        public int ClientId { get; set; }
+        public IEnumerable<OrderCreateDetail> Items { get; set; } = new List<OrderCreateDetail>();
+    }
+
+    public class OrderCreateDetail
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
